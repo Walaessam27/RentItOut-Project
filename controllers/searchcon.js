cc6ef1a7f975825d41e57c7e163b427d8b3a5f18
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2024-10-12 21:52:21 +0300

    create controller folder commit

---
+++ controllers/searchcon.js
@@ -0,0 +1,24 @@
+const Item = require('../models/item'); 
+const { Op } = require('sequelize');  
+
+const searchItems = async (req, res) => {
+    try {
+        const { category, keyword } = req.query;
+
+        
+        const items = await Item.findAll({
+            where: {
+                category: category,
+                description: {
+                    [Op.iLike]: `%${keyword}%` 
+                }
+            }
+        });
+
+        res.json(items);  
+    } catch (error) {
+        res.status(500).json({ error: 'Error while searching for items' });
+    }
+};
+
+module.exports = { searchItems };
---
+++ routes/search.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { searchItems } = require('../controllers/searchcon');
+
+
+router.get('/search', searchItems);
+
+module.exports = router;
