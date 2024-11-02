d2c087268882c438bf160fddde7fdf9952e0881d
Raghadbdair <160124490+Raghadbdair@users.noreply.github.com>
2024-11-02 14:35:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const sequelize = require('./db');
 const itemsRoute = require('./routes/items'); // Items route
 const searchRoute = require('./routes/search'); // Search route
 const rentalRoutes = require('./routes/rentalRoutes'); 
+const paymentRoute = require('./routes/payment');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/api/items', itemsRoute);  // Item routes
 app.use('/api/search', searchRoute);  // Search routes
 app.use('/api/rent', rentalRoutes);
+app.use('/api/payments', paymentRoute);
 
 sequelize.authenticate()
     .then(() => console.log('Database connected...'))
