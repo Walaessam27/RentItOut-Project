c3058ceba17c9f8dd5d74a80436bb6e461ada14b
Raghadbdair <160124490+Raghadbdair@users.noreply.github.com>
2024-11-03 20:49:22 +0200

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const Rental = require('./rental');
+const Item = require('./items');
+const User = require('./user');
+const Payment = require('./payment');  
+const Security = require('./Security');  
+
+module.exports = {
+    Rental,
+    Item,
+    User,
+    Payment ,
+    Security
+
+};
+
