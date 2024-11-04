2da30ad59376c6538fd4555a99852924f215b9c1
Doaa Jararaa <djararaa@gmail.com>
2024-11-04 15:41:30 +0200

    add a API's for rate, update the route and index

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const sequelize = require('./db');
 const itemsRoute = require('./routes/items'); // Items route
 const searchRoute = require('./routes/search'); // Search route
+const ratingRoutes = require('./ratingRoutes'); // Adjust path if needed
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json()); 
 app.use('/api/items', itemsRoute);  // Item routes
 app.use('/api/search', searchRoute);  // Search routes
+app.use('/api/rate', ratingRoutes); // Rate routes
+
 
 sequelize.authenticate()
     .then(() => console.log('Database connected...'))
@@ -17,3 +21,4 @@ sequelize.authenticate()
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
---
+++ routes/RatingRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const RatingController = require('./controllers/RatingController');
+
+// Route to POST a new rating
+router.post('/ratings', RatingController.addrate);
+
+// Route to PUT (update) an existing rating by ID
+router.put('/ratings/:id', RatingController.updateRate);
+
+module.exports = router;
