ee24a64e9abf04736b4cad0438473402d4d1feb6
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2024-11-06 22:51:53 +0200

    delete rating & location files

--- routes/locationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const locationController = require('../controllers/locationController');
-
-router.post('/calculate-distance', locationController.calculateDistance);
-
-module.exports = router;
